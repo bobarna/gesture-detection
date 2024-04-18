94ac89a0e7e21b773f0128c31da6d04c986ed4fc
Justin <justinwit00@gmail.com>
2024-04-18 14:56:27 -0400

    commented out code for plotting training

--- shape_detector/train_model.py
+++ shape_detector/train_model.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split 
 from models.SimpleModel import SimpleModel
 
+from matplotlib import pyplot as plt
+
 
 def load_data():
     shapes = os.listdir('data')
@@ -36,16 +38,25 @@ def main():
 
     # define the model
     model = SimpleModel()
-    model_name = "fist_model"
+    model_name = "results"
+
+    # track loss and accuracy
+    loss_train_values = []
+    loss_test_values = []
+    acc_train_values = []
+    acc_test_values = []
+
     
     # train the model
     loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.0001)
     
-    n_epochs = 250
+    n_epochs = 50
     batch_size = 100
     
     for epoch in range(n_epochs):
+        # trainlosstotal = 0
+        # trainacctotal = 0
         for i in range(0, X_train.shape[0], batch_size):
             Xbatch = X_train[i:i+batch_size]
             y_pred = model(Xbatch)
@@ -54,17 +65,56 @@ def main():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        print(f'Finished epoch {epoch}, latest loss {loss}')
+
+    #         trainlosstotal += loss
+    #         trainacctotal += torch.sum(torch.argmax(y_pred, axis=1) == ybatch) / ybatch.shape[0]
+
+    #     # compute accuracy (no_grad is optional)
+    #     with torch.no_grad():
+    #         trainlosstotal /= X_train.shape[0] // batch_size
+    #         trainacctotal /= X_train.shape[0] // batch_size
+
+    #         y_pred = model(X_test)
+    #         test_loss = loss_fn(y_pred, y_test)
+    #         y_pred = torch.argmax(y_pred, axis=1)
+    #         accuracy = torch.sum(y_pred == y_test) / y_pred.shape[0]
+
+    #     # breakpoint()
+    #     # add avg values to tracking
+    #     loss_train_values.append(trainlosstotal.item())
+    #     loss_test_values.append(test_loss.item())
+    #     acc_train_values.append(trainacctotal.item())
+    #     acc_test_values.append(accuracy.item())
+    #     print(f'Finished epoch {epoch}, latest loss {loss}')
     
-    # compute accuracy (no_grad is optional)
-    with torch.no_grad():
-        y_pred = model(X_test)
-        y_pred = torch.argmax(y_pred, axis=1)
+    # # compute accuracy (no_grad is optional)
+    # with torch.no_grad():
+    #     y_pred = model(X_test)
+    #     y_pred = torch.argmax(y_pred, axis=1)
 
     accuracy = torch.sum(y_pred == y_test) / y_pred.shape[0]
     print(f"Accuracy: {accuracy:.2f}")
 
-    torch.save(model.state_dict(), os.path.join("models", "saved_models", model_name))
+    # torch.save(model.state_dict(), os.path.join("models", "saved_models", model_name))
+
+    # plt.figure(1)
+    # plt.subplot(121)
+    # plt.plot(loss_train_values, label="Train")
+    # plt.plot(loss_test_values, label="Test")
+    # plt.title("Loss Plot")
+    # plt.xlabel("Epoch")
+    # plt.ylabel("Loss")
+    # plt.legend(loc="upper right")
+
+    # plt.subplot(122)
+    # plt.plot(acc_train_values, label="Train")
+    # plt.plot(acc_test_values, label="Test")
+    # plt.title("Accuracy Plot")
+    # plt.xlabel("Epoch")
+    # plt.ylabel("Accuracy")
+    # plt.legend(loc="lower right")
+    # plt.show()
+
 
 if __name__ == "__main__":
     main()
